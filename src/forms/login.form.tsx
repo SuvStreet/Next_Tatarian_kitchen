210c7cd61f1488892618203f3015be5eb95efcdb
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-08-14 16:09:57 +0300

    feat: Создать форму Авторизации.

---
+++ src/forms/login.form.tsx
@@ -0,0 +1,72 @@
+import { Button, Form, Input } from '@heroui/react'
+import { useState } from 'react'
+
+type IProps = {
+  onClose: () => void
+}
+
+const LoginForm = ({ onClose }: IProps) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('Форма отправлена', formData)
+
+    onClose()
+  }
+
+  return (
+    <Form className="w-full" onSubmit={handleSubmit}>
+      <Input
+        aria-label="Email"
+        isRequired
+        name="email"
+        placeholder="Введите email"
+        type="email"
+        value={formData.email}
+        classNames={{
+          inputWrapper: 'bg-default-100',
+          input: 'text-sm focus:outline-none ',
+        }}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        validate={(value) => {
+          if (!value) return 'Почта обязательна'
+          return null
+        }}
+      />
+
+      <Input
+        isRequired
+        name="password"
+        placeholder="Введите пароль"
+        type="password"
+        value={formData.password}
+        classNames={{
+          inputWrapper: 'bg-default-100',
+          input: 'text-sm focus:outline-none ',
+        }}
+        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+        validate={(value) => {
+          if (!value) return 'Пароль обязателен'
+          if (value.length < 6) return 'Пароль должен быть не менее 6 символов'
+          return null
+        }}
+      />
+
+      <div className="flex w-[100%]  gap-4 items-center pt-8 justify-end">
+        <Button variant="light" onPress={onClose}>
+          Отмена
+        </Button>
+        <Button color="primary" type="submit">
+          Войти
+        </Button>
+      </div>
+    </Form>
+  )
+}
+
+export default LoginForm
